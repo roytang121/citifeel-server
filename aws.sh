b9b3132317f0be27196226099a01a49d1b21f0fc
Jason Ng <ywng@ust.hk>
2014-04-26 22:57:48 +0800

    default

--- aws.sh
+++ aws.sh
@@ -2,12 +2,12 @@ git add *
 read -p "Default commit message? y/n " default
 
 if [ "$default" == "Y" ] || [ "$default" == "y" ]; then
-	message = "default"
+	message="default"
 else
 	read -p "Please input your commit message: " message
 fi
 
 git commit -m $message
 
-git push 
+git push origin master
 git push openshift master

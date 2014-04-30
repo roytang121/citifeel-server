689b9ec6b6a04d20be77e94079f54fce3d83436d
Jason Ng <ywng@ust.hk>
2014-04-30 23:18:37 +0800

    default

---
+++ php/api/application/config/error_message.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+// Please add your custom error here
+// format: $config['error'][__number__] = '__Your Error Message__';
+
+$config['error'][0] = 'UNKNOWN ERROR.'; // do not use this except CORE_Controller
+$config['error'][1] = 'Failed credential check.';
+$config['error'][2] = 'Standard validation error, you should use validation_error() function to generate exact message back to the app.';
+
+// XX related error message
+$config['error'][3] = '';
+$config['error'][4] = '';
+
+
+
+/* End of file error_message.php */
+/* Location: ./application/config/error_message.php */

cedf6baafcd5f6eae758bfbf8b79414490947ec3
Jason Ng <ywng@ust.hk>
2014-05-09 15:41:21 +0800

    default

--- php/api/application/controllers/user.php
+++ php/api/application/controllers/user.php
@@ -18,7 +18,7 @@ class User extends REST_Controller {
 		$CI->config->load("facebook",TRUE);
 		$config = $CI->config->item('facebook');
 		$this->load->library('Facebook', $config);
-		$this->load->library('Base_facebook',$config);
+		$this->load->library('base_facebook',$config);
 	
 	}
 
@@ -62,7 +62,7 @@ class User extends REST_Controller {
 	
         // Try to get the user's id on Facebook
         $accessToken_fb=$this->input->post('access_token');
-        $this->base_facebook->setAccessToken($accessToken_fb);
+        $this->BaseFacebook->setAccessToken($accessToken_fb);
         $userId = $this->facebook->getUser();
  
         // If user is not yet authenticated, the id will be zero

b7f3a0a8799f6a8a093f461ca56aed84d13b992c
benleung <cpleungaa@stu.ust.hk>
2014-07-06 03:09:14 +0800

    .

--- php/api/application/controllers/user.php
+++ php/api/application/controllers/user.php
@@ -9,6 +9,8 @@ require_once (APPPATH. 'libraries/REST_Controller.php');
 
 class User extends REST_Controller {
 
+	var $user_type = '';
+
 	public function __construct() {
 		parent::__construct();
 		$this->load->library('CORE_Controller');
@@ -21,7 +23,7 @@ class User extends REST_Controller {
 	
 	}
 
-	var $user_type = '';
+	
 
 	public function index()
 	{

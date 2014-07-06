96aaf1207be7f13e3c70eeb58b846e5539a307cb
benleung <cpleungaa@stu.ust.hk>
2014-07-06 21:58:02 +0800

    .

--- php/api/application/libraries/REST_Controller.php
+++ php/api/application/libraries/REST_Controller.php
@@ -904,8 +904,9 @@ abstract class REST_Controller extends CI_Controller
 	public function validation_errors()
 	{
 		//$string = strip_tags($this->form_validation->error_string());
-		$string = 'haha';
-		return explode("\n", trim($string, "\n"));
+		//$string = 'haha';
+		//return explode("\n", trim($string, "\n"));
+		return array('a','b');
 	}
 
 	// SECURITY FUNCTIONS ---------------------------------------------------------

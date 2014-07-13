084610be2c07faab503c61cc9fb1183a6be3bd3d
benleung <cpleungaa@stu.ust.hk>
2014-07-13 18:12:41 +0800

    .

--- php/api/application/controllers/post.php
+++ php/api/application/controllers/post.php
@@ -38,7 +38,7 @@ class Post extends REST_Controller {
 
 	
 	/**
-	*  INPUT: user_id, caption, company_id, rating, price, url, region
+	*  INPUT (7): user_id, caption, company_id, rating, price, url, region
 	*/
 	public function create_post()
 	{
@@ -61,15 +61,14 @@ class Post extends REST_Controller {
 		// Create Post
 		$this->load->model('post_model');
 		$data = array(
-                $this->user_model->KEY_post_id = $this->input->post('post_id'),
-				$this->user_model->KEY_user_id = $this->input->post('user_id'),
-				$this->user_model->KEY_caption = $this->input->post('caption'),
-				$this->user_model->KEY_company_id = $this->input->post('company_id'),
-				$this->user_model->KEY_rating = $this->input->post('rating'),
-				$this->user_model->KEY_post_time = $this->input->post('post_time'),
-				$this->user_model->KEY_price = $this->input->post('price'),
-				$this->user_model->KEY_url = $this->input->post('url'),
-				$this->user_model->KEY_region = $this->input->post('region')
+			$this->user_model->KEY_user_id = $this->input->post('user_id'),
+			$this->user_model->KEY_caption = $this->input->post('caption'),
+			$this->user_model->KEY_company_id = $this->input->post('company_id'),
+			$this->user_model->KEY_rating = $this->input->post('rating'),
+			$this->user_model->KEY_post_time = date('Y-m-d G:i:s'),
+			$this->user_model->KEY_price = $this->input->post('price'),
+			$this->user_model->KEY_url = $this->input->post('url'),
+			$this->user_model->KEY_region = $this->input->post('region')
         );
 		
 		$this->user_model->create_post($data);
